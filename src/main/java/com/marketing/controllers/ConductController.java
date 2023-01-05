67259475913b2957b7ce049131304d8c1da55775
nisha kothare <53811249+nisha555@users.noreply.github.com>
2023-01-05 12:42:20 +0530

    done

---
+++ src/main/java/com/marketing/controllers/ConductController.java
@@ -0,0 +1,14 @@
+package com.marketing.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConductController {
+	
+	@RequestMapping("/hi")
+	public int viewMsg() {
+		return 10 ;
+	}
+
+}
