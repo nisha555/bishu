05606161307c777a5075f877fb9e3bab091148ee
nisha kothare <53811249+nisha555@users.noreply.github.com>
2023-01-05 13:52:53 +0530

    vicky commited

--- src/main/java/com/marketing/controllers/ConductController.java
+++ src/main/java/com/marketing/controllers/ConductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //controller
 //apoorva friend
 //sudhansu//dtythrytrythhd
+//vicky boy
 
 public class ConductController {
 	@RequestMapping("/hi")
